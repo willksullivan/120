bbbb7ab4f81ce5a5291af07f59e7fdd53440dbc5
willksullivan <36898585+willksullivan@users.noreply.github.com>
2018-04-11 20:10:31 -0500

    Put in notes

--- JollyJackpotLand/JollyJackpotLand/Program.cs
+++ JollyJackpotLand/JollyJackpotLand/Program.cs
@@ -8,14 +8,30 @@ namespace JollyJackpotLand
         {
             /*
              * begin with 500 gill
-             * continue to play until user hits 5000
+             * continue to play until user hits 1000
              * MAIN MENU
              * slot machine
              * black jack
              *roulette wheel
              * save
              */
-            Console.WriteLine("Welcome to jolly jackpot land");
+            int gil = 0;
+            do
+            {
+                //login
+                // - new user
+                // - load user
+
+                //main menu
+                // --  choose game
+                // -- opportunity to exit
+
+                //slot
+
+                //blackjack
+
+                //roulette
+            } while (gil <= 1000);
         }
     }
 }
